ef9d860ae3ebe66e746853630abb8af20af6fe14
Kasea <1997xeela@gmail.com>
2018-06-09 17:58:49 +0200

    Credits for AAHM to *****(dunno if he wanna be named)

--- guides/9920.js
+++ guides/9920.js
@@ -1,145 +1,17 @@
 // AAHM
 
 module.exports = {
-    // First boss
+//Third Boss
+"s-920-3000-1109": [{"type": "text","sub_type": "notification","message": " Spin-> left side safe >Out-in"}],
+"s-920-3000-1111": [{"type": "text","sub_type": "notification","message": " Spin-> right side safe >in-out"}],
+"s-920-3000-1113": [{"type": "text","sub_type": "notification","message": " Front hammer into Back hammer "}],
+"s-920-3000-1115": [{"type": "text","sub_type": "notification","message": " Spin"}],
+"s-920-3000-1202": [{"type": "text","sub_type": "notification","message": " BACKSTAB"}],
+"s-920-3000-1205": [{"type": "text","sub_type": "notification","message": " BACKSTAB - 2"}],
+"s-920-3000-1206": [{"type": "text","sub_type": "notification","message": " BACKSTAB - 3"}],
 
-    // Target laser?
-    "s-920-1000-1223": [
-        {
-            "type": "text",
-            "sub_type": "message",
-            "message": "Go to ice(if secondary aggro)"
-        }
-    ],
-
-    // Push back -> target?
-    "s-920-1000-1226": [
-        {
-            "type": "text",
-            "sub_type": "message",
-            "message": "Iframe push back"
-        }
-    ],
-
-    // Shield back swing (unenraged)
-    "s-920-1000-1105": [
-        {
-            "type": "text",
-            "sub_type": "message",
-            "message": "Dodge swing back attack"
-        }
-    ],
-
-    // Shield back swing (enraged)
-    "s-920-1000-2105": [
-        {
-            "type": "text",
-            "sub_type": "message",
-            "message": "Dodge swing back attack"
-        },
-        {
-            "type": "text",
-            "delay": 1000,
-            "sub_type": "message",
-            "message": "Back jump"
-        },
-        {
-            "type": "text",
-            "delay": 1300,
-            "sub_type": "message",
-            "message": "Out/In (iframe)"
-        }
-    ],
-
-    // Second boss -- honestly idk with this one, names are all over the place
-
-    // Boss below 50%
-    "h-920-2000-50": [
-        {
-            "type": "text",
-            "sub_type": "notification",
-            "message": "Boss 50%. Both slashes from now on."
-        }
-    ],
-
-    // Boss below 20%
-    "h-920-2000-20": [
-        {
-            "type": "text",
-            "sub_type": "notification",
-            "message": "Boss 20%. Debuff often."
-        }
-    ],
-
-    // Back attack + line on left ?
-    "s-920-2000-1113": [
-        {
-            "type": "text",
-            "sub_type": "message",
-            "message": "Dodge back left swing (line)"
-        },
-        {
-            "type": "spawn",
-            "id": 559,
-            "sub_delay": 5000,
-            "distance": 200,
-            "offset": -1.57
-        },
-    ],
-
-    // Back attack + line on right ?
-    "s-920-2000-1114": [
-        {
-            "type": "text",
-            "sub_type": "message",
-            "message": "Dodge back right swing (line)"
-        },
-        {
-            "type": "spawn",
-            "id": 559,
-            "sub_delay": 5000,
-            "distance": 200,
-            "offset": 1.57
-        },
-    ],
-
-    // Spinning attack (unenraged)
-    "s-920-2000-1106": [
-        {
-            "type": "text",
-            "sub_type": "message",
-            "message": "Spinning attack(iframe, 15m)"
-        },
-    ],
-
-    // Spinning attack (enraged)
-    "s-920-2000-2106": [
-        {
-            "type": "text",
-            "sub_type": "message",
-            "message": "Spinning attack(iframe, 15m)"
-        }
-    ],
-
-
-    // Third boss -- Idk names were aids :shrug: (only looked until 50% before I got bored)
-
-    // Double cut -> double cut -> back attack (unenraged) ?
-    "s-920-3000-1203": [
-        {
-            "type": "text",
-            "sub_type": "message",
-            "message": "Prepare to iframe(back attack)"
-        }
-    ],
-
-    // Double cut -> double cut -> back attack (enraged) ?
-    "s-920-3000-2203": [
-        {
-            "type": "text",
-            "sub_type": "message",
-            "message": "Prepare to iframe(back attack)"
-        }
-    ],
-
+//Boss Enraged
+"s-920-3000-2109": [{"type": "text","sub_type": "notification","message": " Spin-> left side safe >Out-in"}],
+"s-920-3000-2111": [{"type": "text","sub_type": "notification","message": " Spin-> right side safe >in-out"}],
+"s-920-3000-2113": [{"type": "text","sub_type": "notification","message": " Front hammer into Back hammer"}],
 };
