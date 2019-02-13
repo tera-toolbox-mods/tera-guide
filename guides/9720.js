2de3121fd30c8dc96b5c17c74be1680dc90c1db7
mc <46639858+michengs@users.noreply.github.com>
2019-02-13 20:25:40 +0800

    Add files via upload

--- guides/9720.js
+++ guides/9720.js
@@ -15,230 +15,230 @@ const ITEM_SPAWNED_ON_SWIPE_DISTANCE = 150;
 
 const EVENT_DELAY_FIRST_BOSS_NM = [
 {
-        "type": "text",
-        "sub_type": "message",
-        "delay": TIMER_DELAY,
-        "message": "Dodge!",
-		"message_TW": "&#x6CE8;&#x610F;&#x8EB2;&#x907F;"
-    },
+"type": "text",
+"sub_type": "message",
+"delay": TIMER_DELAY,
+"message": "Dodge!",
+"message_TW": "注意躲避"
+},
 ];
 
 //三王后砸前置计数
 // thirdboss: counter of back attacks
 
-   function back_attack_NM(handlers) {
-	  clearTimeout(timer);
-      counter++;
-    if(counter >= 2) {
-        handlers['text']({
-            "sub_type": "message",
-			"message": "Back attack",
-            "message_TW": "&#x540E;&#x7838;"
-        });
-    }
-    timer = setTimeout(()=>{
-        counter = 0;
-    }, 3000);
+function back_attack_NM(handlers) {
+clearTimeout(timer);
+counter++;
+if(counter >= 2) {
+handlers['text']({
+"sub_type": "message",
+"message": "Back attack",
+"message_TW": "后砸"
+});
+}
+timer = setTimeout(()=>{
+counter = 0;
+}, 3000);
 }
 
 module.exports = {
 
- "h-720-1000-100": [{"type": "text","sub_type": "notification","delay": 2000,"message": "proxy频道输入:補助 help <br>获取更多使用信息!"}],
+"h-720-1000-100": [{"type": "text","sub_type": "notification","delay": 2000,"message": "proxy频道输入:補助 help <br>获取更多使用信息!"}],
 
 //一王 
 //firstboss
 
-    //后跳+内外圈,安全区域：站里面+站外面
-	//Backstep+donuts, stay in, get out
-	
-     "s-720-1000-117-0": [{"type": "text","sub_type": "message","message": "stay in&#x2191; + get out&#x2193;","message_TW": "&#x7AD9;&#x91CC;&#x9762;&#x2191;+&#x7AD9;&#x5916;&#x9762;&#x2193;"}],
-	
-	//原地抬腿+内外圈,安全区域：站外面+站里面
-	//Stomp+donuts, get out, stay in
-	
-	 "s-720-1000-116-0": [{"type": "text","sub_type": "message","message": "get out&#x2193; + stay in&#x2191;","message_TW": "&#x7AD9;&#x5916;&#x9762;&#x2193;+&#x7AD9;&#x91CC;&#x9762;&#x2191;"}],
-    
-	//剑戳地2下+后方挥盾
-	//ground thrust 2x+shield swing
-	
-	 "s-720-1000-109-0": [{"type": "text","sub_type": "message","message": "back attack","message_TW": "&#x540E;&#x65B9;&#x653B;&#x51FB;"}],
+//后跳+内外圈,安全区域：站里面+站外面
+//Backstep+donuts, stay in, get out
+
+"s-720-1000-117-0": [{"type": "text","sub_type": "message","message": "stay in↑ + get out↓","message_TW": "站里面↑+站外面↓"}],
+
+//原地抬腿+内外圈,安全区域：站外面+站里面
+//Stomp+donuts, get out, stay in
+
+"s-720-1000-116-0": [{"type": "text","sub_type": "message","message": "get out↓ + stay in↑","message_TW": "站外面↓+站里面↑"}],
+
+//剑戳地2下+后方挥盾
+//ground thrust 2x+shield swing
+
+"s-720-1000-109-0": [{"type": "text","sub_type": "message","message": "back attack","message_TW": "后方攻击"}],
 
 
 
 //firstboss special attack
-	 
-	 //击飞攻击
-	 //knockup attack
-	 
-	 "s-720-1000-300-0": EVENT_DELAY_FIRST_BOSS_NM,
+
+//击飞攻击
+//knockup attack
+
+"s-720-1000-300-0": EVENT_DELAY_FIRST_BOSS_NM,
 
 //二王 
 //secondboss, 
 
-    //转圈攻击（1106）
-	//Spin attack
-	
-	"s-720-2000-106-0": [{"type": "text","sub_type": "message","message": "Spin attack","message_TW": "&#x8F6C;&#x5708;&#x653B;&#x51FB;"}],
-	
-    //后方攻击（1105）
-	//Back attack
-	
-    "s-720-2000-105-0": [{"type": "text","sub_type": "message","message": "Back attack","message_TW": "&#x540E;&#x65B9;&#x653B;&#x51FB;"}],
-	
-	//点名大跳晕人
-	//Random aggro stun
-	
-    "s-720-2000-104-0": [{"type": "text","sub_type": "message","message": "Random jump","message_TW": "&#x70B9;&#x540D;&#x5927;&#x8DF3;"}],
-   
-   //眩晕攻击
-   //Stun attack
-   
-	"s-720-2000-110-0": [{"type": "text","sub_type": "message","message": "Stun attack","message_TW": "&#x7729;&#x6655;&#x653B;&#x51FB;"}],
-	
-	//BOSS右手边划刀,安全区域：坦右,打左
-	//right hand side swing, tank goes to right, dps goes to left
+//转圈攻击（1106）
+//Spin attack
 
-	"s-720-2000-112-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "right&#x2192;","message_TW": "&#x53F3;&#x2192;"},
-	                    {"type": "text","class_position":"dps","sub_type": "message","message": "left&#x2190;","message_TW": "&#x5DE6;&#x2190;"},
-	                    {"type": "text","class_position":"heal","sub_type": "message","message": "left&#x2190;","message_TW": "&#x5DE6;&#x2190;"}],
+"s-720-2000-106-0": [{"type": "text","sub_type": "message","message": "Spin attack","message_TW": "转圈攻击"}],
 
-	//BOSS左手边划刀,安全区域：坦左,打右
-	//left hand side swing, tank goes to left, dps goes to right
-	
-	"s-720-2000-111-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "left&#x2190;","message_TW": "&#x5DE6;&#x2190;"},
-	                    {"type": "text","class_position":"dps","sub_type": "message","message": "right&#x2192;","message_TW": "&#x53F3;&#x2192;"},
-	                    {"type": "text","class_position":"heal","sub_type": "message","message": "right&#x2192;","message_TW": "&#x53F3;&#x2192;"}],
+//后方攻击（1105）
+//Back attack
+
+"s-720-2000-105-0": [{"type": "text","sub_type": "message","message": "Back attack","message_TW": "后方攻击"}],
+
+//点名大跳晕人
+//Random aggro stun
+
+"s-720-2000-104-0": [{"type": "text","sub_type": "message","message": "Random jump","message_TW": "点名大跳"}],
+
+//眩晕攻击
+//Stun attack
+
+"s-720-2000-110-0": [{"type": "text","sub_type": "message","message": "Stun attack","message_TW": "眩晕攻击"}],
+
+//BOSS右手边划刀,安全区域：坦右,打左
+//right hand side swing, tank goes to right, dps goes to left
+
+"s-720-2000-112-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "right→","message_TW": "右→"},
+{"type": "text","class_position":"dps","sub_type": "message","message": "left←","message_TW": "左←"},
+{"type": "text","class_position":"heal","sub_type": "message","message": "left←","message_TW": "左←"}],
+
+//BOSS左手边划刀,安全区域：坦左,打右
+//left hand side swing, tank goes to left, dps goes to right
+
+"s-720-2000-111-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "left←","message_TW": "左←"},
+{"type": "text","class_position":"dps","sub_type": "message","message": "right→","message_TW": "右→"},
+{"type": "text","class_position":"heal","sub_type": "message","message": "right→","message_TW": "右→"}],
 
 
 //二王特殊技能
 //secondboss, special attacks
 
-    //红色鉴定,安全区域：15M外
-	//Red, stay 15m away
-	
-	"s-720-2000-119-0": [{"type": "text","sub_type": "message","message": "red: get out&#x2193;","message_TW": "&#x7EA2;&#x8272;&#xFF1A;&#x5916;&#x2193;"}],
-	
-    //蓝色鉴定,安全区域：15M内（3220）
-	//Blue, stay near within 15m
-	
-	"s-720-2000-220-0": [{"type": "text","sub_type": "message","message": "blue: stay in&#x2191;","message_TW": "&#x84DD;&#x8272;&#xFF1A;&#x5185;&#x2191;"}],
-	
-    //眩晕圈（3116）+外圈伤害（3118）
-	//stun+donut
-    "s-720-2000-116-0": [{"type": "text","sub_type": "message","message": "dodge + stay in&#x2191;","message_TW": "&#x8EB2;&#x907F;+&#x5185;&#x2191;"}],
-	
-	//点名喷长条毒（3107）
-	//random aggro poison（3107）
+//红色鉴定,安全区域：15M外
+//Red, stay 15m away
+
+"s-720-2000-119-0": [{"type": "text","sub_type": "message","message": "red: get out↓","message_TW": "红色：外↓"}],
+
+//蓝色鉴定,安全区域：15M内（3220）
+//Blue, stay near within 15m
+
+"s-720-2000-220-0": [{"type": "text","sub_type": "message","message": "blue: stay in↑","message_TW": "蓝色：内↑"}],
+
+//眩晕圈（3116）+外圈伤害（3118）
+//stun+donut
+"s-720-2000-116-0": [{"type": "text","sub_type": "message","message": "dodge + stay in↑","message_TW": "躲避+内↑"}],
+
+//点名喷长条毒（3107）
+//random aggro poison（3107）
 
 //三王 不愤怒
 //thirdboss, unenraged
 
-    //进场的推人红圈
-	//pushback when engaging
-	
-     "s-720-3000-315-0": [{"type": "text","sub_type": "message","message": "Pushback","message_TW": "&#x5F00;&#x573A;&#x63A8;&#x4EBA;"}],
-	 
-	//点名大跳晕人（1107）
-	//random aggro stun
-	
-    "s-720-3000-107-0": [{"type": "text","sub_type": "message","message": "Random jump","message_TW": "&#x70B9;&#x540D;&#x5927;&#x8DF3;"}],
-	
-	//点名出剑刃风暴,手里握篮球挥篮球（1120）,剑刃风暴（1121）
-	//random aggro, holding blue orb, energy beam
-	
-  
-	
+//进场的推人红圈
+//pushback when engaging
+
+"s-720-3000-315-0": [{"type": "text","sub_type": "message","message": "Pushback","message_TW": "开场推人"}],
+
+//点名大跳晕人（1107）
+//random aggro stun
+
+"s-720-3000-107-0": [{"type": "text","sub_type": "message","message": "Random jump","message_TW": "点名大跳"}],
+
+//点名出剑刃风暴,手里握篮球挥篮球（1120）,剑刃风暴（1121）
+//random aggro, holding blue orb, energy beam
+
+
+
 //三王 
 //thirdboss
 
-	//点名出剑刃风暴,手里握篮球挥篮球（1204）,剑刃风暴（1121）
-	//random aggro, holding blue orb, energy beam
-	
-    "s-720-3000-204-0": [{"type": "text","sub_type": "message","message": "enraged:energy beam","message_TW": "&#x6124;&#x6012;&#xFF1A;&#x70B9;&#x540D;&#x5251;&#x6C14;"}],
-    
-	//剑舞前戳+逆时针旋转+右手扇形攻击 注：坦右边安全,打左边安全请自行更改
-	//heart thrust+anticlockwise spin+right swipe
-	
-    "s-720-3000-109-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "right&#x2192;","message_TW": "&#x53F3;&#x2192;"},
-	                    {"type": "text","class_position":"dps","sub_type": "message","message": "left&#x2190;","message_TW": "&#x5DE6;&#x2190;"},
-	                    {"type": "text","class_position":"heal","sub_type": "message","message": "left&#x2190;","message_TW": "&#x5DE6;&#x2190;"},
-	
-	// Courtesy of Kasea ;)
-        {
-            "type": "spawn",
-            "id": ITEM_SPAWNED_ON_SWIPE_ID,
-            "sub_delay": ITEM_SPAWNED_ON_SWIPE_SUB_DELAY,
-            "distance": ITEM_SPAWNED_ON_SWIPE_DISTANCE,
-            "offset": -1
-        },
-        {
-            "type": "spawn",
-            "id": ITEM_SPAWNED_ON_SWIPE_ID,
-            "sub_delay": ITEM_SPAWNED_ON_SWIPE_SUB_DELAY,
-            "distance": ITEM_SPAWNED_ON_SWIPE_DISTANCE,
-            "offset": -2.3
-        }
-    ],
-	
-    //剑舞前戳+顺时针旋转+左手扇形攻击 注：坦左边安全,打右边安全请自行更改
-	//heart thrust+clockwise spin+left swipe
-	
-	"s-720-3000-111-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "left&#x2190;","message_TW": "&#x5DE6;&#x2190;"},
-	                    {"type": "text","class_position":"dps","sub_type": "message","message": "right&#x2192;","message_TW": "&#x53F3;&#x2192;"},
-	                    {"type": "text","class_position":"heal","sub_type": "message","message": "right&#x2192;","message_TW": "&#x53F3;&#x2192;"},
-	
-	// Courtesy of Kasea ;)
-        {
-            "type": "spawn",
-            "id": ITEM_SPAWNED_ON_SWIPE_ID,
-            "sub_delay": ITEM_SPAWNED_ON_SWIPE_SUB_DELAY,
-            "distance": ITEM_SPAWNED_ON_SWIPE_DISTANCE,
-            "offset": 1
-        },
-        {
-            "type": "spawn",
-            "id": ITEM_SPAWNED_ON_SWIPE_ID,
-            "sub_delay": ITEM_SPAWNED_ON_SWIPE_SUB_DELAY,
-            "distance": ITEM_SPAWNED_ON_SWIPE_DISTANCE,
-            "offset": 2.3
-        }
-    ],
+//点名出剑刃风暴,手里握篮球挥篮球（1204）,剑刃风暴（1121）
+//random aggro, holding blue orb, energy beam
 
-    //前后砸
-	//front, back slam
-	
-	"s-720-3000-113-0": [{"type": "text","sub_type": "message","message": "front, back slam","message_TW": "&#x524D;&#x540E;&#x7838;"}],
-	
-    //愤怒2连斜上挥（2104）后接的后砸（2119）
-	//golf swing x2 + back slam
-	"s-720-3000-104-0": [{"type": "func","func": back_attack_NM}],
-	
-    //旋转攻击
-	//spinning attack
-	
-	"s-720-3000-115-0": [{"type": "text","sub_type": "message","message": "spinning attack","message_TW": "&#x65CB;&#x8F6C;&#x653B;&#x51FB;"}],
-	
-	//点名大跳晕人（1107）
-	//random aggro stun
-	
-    "s-720-3000-107-0": [{"type": "text","sub_type": "message","message": "Random jump","message_TW": "&#x70B9;&#x540D;&#x5927;&#x8DF3;"}],
-	
-    //后闪+旋转or前后砸
-	//teleport back+ spin or front, back slam
-	
-	"s-720-3000-202-0": [{"type": "text","sub_type": "message","message": "spin or front,back slam","message_TW": "&#x65CB;&#x8F6C;or&#x524D;&#x540E;&#x7838;"}],
+"s-720-3000-204-0": [{"type": "text","sub_type": "message","message": "enraged:energy beam","message_TW": "愤怒：点名剑气"}],
+
+//剑舞前戳+逆时针旋转+右手扇形攻击 注：坦右边安全,打左边安全请自行更改
+//heart thrust+anticlockwise spin+right swipe
+
+"s-720-3000-109-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "right→","message_TW": "右→"},
+{"type": "text","class_position":"dps","sub_type": "message","message": "left←","message_TW": "左←"},
+{"type": "text","class_position":"heal","sub_type": "message","message": "left←","message_TW": "左←"},
+
+// Courtesy of Kasea ;)
+{
+"type": "spawn",
+"id": ITEM_SPAWNED_ON_SWIPE_ID,
+"sub_delay": ITEM_SPAWNED_ON_SWIPE_SUB_DELAY,
+"distance": ITEM_SPAWNED_ON_SWIPE_DISTANCE,
+"offset": -1
+},
+{
+"type": "spawn",
+"id": ITEM_SPAWNED_ON_SWIPE_ID,
+"sub_delay": ITEM_SPAWNED_ON_SWIPE_SUB_DELAY,
+"distance": ITEM_SPAWNED_ON_SWIPE_DISTANCE,
+"offset": -2.3
+}
+],
+
+//剑舞前戳+顺时针旋转+左手扇形攻击 注：坦左边安全,打右边安全请自行更改
+//heart thrust+clockwise spin+left swipe
+
+"s-720-3000-111-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "left←","message_TW": "左←"},
+{"type": "text","class_position":"dps","sub_type": "message","message": "right→","message_TW": "右→"},
+{"type": "text","class_position":"heal","sub_type": "message","message": "right→","message_TW": "右→"},
+
+// Courtesy of Kasea ;)
+{
+"type": "spawn",
+"id": ITEM_SPAWNED_ON_SWIPE_ID,
+"sub_delay": ITEM_SPAWNED_ON_SWIPE_SUB_DELAY,
+"distance": ITEM_SPAWNED_ON_SWIPE_DISTANCE,
+"offset": 1
+},
+{
+"type": "spawn",
+"id": ITEM_SPAWNED_ON_SWIPE_ID,
+"sub_delay": ITEM_SPAWNED_ON_SWIPE_SUB_DELAY,
+"distance": ITEM_SPAWNED_ON_SWIPE_DISTANCE,
+"offset": 2.3
+}
+],
+
+//前后砸
+//front, back slam
+
+"s-720-3000-113-0": [{"type": "text","sub_type": "message","message": "front, back slam","message_TW": "前后砸"}],
+
+//愤怒2连斜上挥（2104）后接的后砸（2119）
+//golf swing x2 + back slam
+"s-720-3000-104-0": [{"type": "func","func": back_attack_NM}],
+
+//旋转攻击
+//spinning attack
+
+"s-720-3000-115-0": [{"type": "text","sub_type": "message","message": "spinning attack","message_TW": "旋转攻击"}],
+
+//点名大跳晕人（1107）
+//random aggro stun
+
+"s-720-3000-107-0": [{"type": "text","sub_type": "message","message": "Random jump","message_TW": "点名大跳"}],
+
+//后闪+旋转or前后砸
+//teleport back+ spin or front, back slam
+
+"s-720-3000-202-0": [{"type": "text","sub_type": "message","message": "spin or front,back slam","message_TW": "旋转or前后砸"}],
 
 //三王特殊技能
 //thirdboss, special attacks
- 
-	//召唤分身出点名剑气
-	//clones, random aggro, energy beam
-	
-	"s-720-3000-400-0": [{"type": "text","sub_type": "message","message": "beam","message_TW": "&#x53EC;&#x5484;&#x5206;&#x8EAB;&#xFF1A;&#x70B9;&#x540D;&#x5251;&#x6C14;"}],
-	
-	//召唤分身出旋转攻击
-	//clones, random aggro, spin attack
-	
-    "s-720-3000-401-0": [{"type": "text","sub_type": "message","message": "spin","message_TW": "&#x53EC;&#x5484;&#x5206;&#x8EAB;&#xFF1A;&#x65CB;&#x8F6C;&#x653B;&#x51FB;"}]
+
+//召唤分身出点名剑气
+//clones, random aggro, energy beam
+
+"s-720-3000-400-0": [{"type": "text","sub_type": "message","message": "beam","message_TW": "召咄分身：点名剑气"}],
+
+//召唤分身出旋转攻击
+//clones, random aggro, spin attack
+
+"s-720-3000-401-0": [{"type": "text","sub_type": "message","message": "spin","message_TW": "召咄分身：旋转攻击"}]
 
 };
