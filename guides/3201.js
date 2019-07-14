8163b637be4a53a4433e5a1122eeba331db21586
mc <46639858+michengs@users.noreply.github.com>
2019-07-14 13:51:42 +0800

    Add files via upload

--- guides/3201.js
+++ guides/3201.js
@@ -1,11 +1,45 @@
 // 
 let notice_guide = true;
 let   notice = true  ; 
-let boss = 01;
+let boss = 3;
 let lastboss = false;
 let timer = 01;
 let player, entity, library, effect;
 let	print = false;
+
+function SpawnThing5( degrees, radius, delay, times, handlers, event, entity ) {
+	let shield_loc = entity['loc'].clone();
+	shield_loc.w = entity['loc'].w;			
+   let angle =  Math.PI * degrees / 180 
+        handlers['spawn']({
+			"sub_type": "build_object",
+        	"id": 1,
+			"delay": delay,			
+        	"sub_delay": times,
+        	"distance": radius,
+        	"offset": angle,
+			"ownerName": "定时炸弹",
+			"message": "定时炸弹"
+        }, {loc: shield_loc});  
+}
+
+function SpawnThing6( degrees, radius, delay, times, handlers, event, entity ) {
+	let shield_loc = entity['loc'].clone();
+	shield_loc.w = entity['loc'].w;			
+   let angle =  Math.PI * degrees / 180 
+        handlers['spawn']({
+			"sub_type": "build_object",
+        	"id": 1,
+			"delay": delay,			
+        	"sub_delay": times,
+        	"distance": radius,
+        	"offset": angle,
+			"ownerName": "炸弹",
+			"message": "炸弹"
+        }, {loc: shield_loc});  
+}
+
+
 
 function guid_voice(handlers) {   
 if(notice_guide) {
@@ -75,7 +109,7 @@ function Spawnitem2(item,degrees,distance, intervalDegrees, radius, times, handl
 function start_boss() {
   lastboss = true  ;
   notice = true  ; 
-  boss = 01;
+  boss = 3;
   print = true ; 
 }
 function start_boss1() {
@@ -124,10 +158,10 @@ setTimeout(function () {
 	if (boss === 1){
 handlers['text']({
 "sub_type": "message",
-"message_TW": "重置buff!",
+"message_TW": "重置debuff!",
 "message": "debuff reload"
 });	
-		boss = 01;
+		boss = 3;
 }
 		   }, 80000);		
 }	
@@ -137,10 +171,10 @@ setTimeout(function () {
 	if (boss === 0){
 handlers['text']({
 "sub_type": "message",
-"message_TW": "重置buff!",
+"message_TW": "重置debuff!",
 "message": "debuff reload"
 });	
-		boss = 01;
+		boss = 3;
 }
 		   }, 80000);			
 }	
@@ -203,7 +237,7 @@ handlers['text']({
 		   }, 55000);	
 }
 
-if (skillid === 203 && boss === 01 ) {
+if (skillid === 203 && boss === 3 ) {
 handlers['text']({
 "sub_type": "message",
 "message_TW": "dps位本体!",
@@ -217,7 +251,7 @@ handlers['text']({
 });	
 		   }, 55000);	
 }
-if (skillid === 204 && boss === 01 ) {
+if (skillid === 204 && boss === 3 ) {
 handlers['text']({
 "sub_type": "message",
 "message_TW": "tank位本体!!",
@@ -231,7 +265,7 @@ handlers['text']({
 });	
 		   }, 55000);	
 }
-if (notice && skillid === 234 && boss === 01 ) {
+if (notice && skillid === 234 && boss === 3 ) {
 handlers['text']({
 "sub_type": "message",
 "message_TW": "dps位本体!!!",
@@ -286,7 +320,7 @@ module.exports = {
     // 1王
     "s-3201-1000-103-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "dodge","message_TW": "闪避!!!" }],	
     "s-3201-1000-104-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Stun attack","message_TW": "眩晕!!!" }],	
-    "s-3201-1000-107-0": [{"type": "text","sub_type": "message","message": "back","message_TW": "后喷（慢）" },
+    "s-3201-1000-107-0": [{"type": "text","sub_type": "message","message": "back","message_TW": "后喷慢" },
 	{"type": "text","sub_type": "message","delay": 2250,"message": "pull","message_TW": "击退!!!"}],	
 	
     "s-3201-1000-111-0": [{"type": "text","sub_type": "message","message": "Ranged DPS attention","message_TW": "远程注意后喷" }],	
@@ -310,8 +344,8 @@ module.exports = {
 	"s-3201-1000-131-0": [{"type": "text","class_position":"dps","sub_type": "message","message": "Ranged DPS attention","message_TW": "远程注意后喷"},
 	                       {"type": "text","class_position":"heal","sub_type": "message","message": "Ranged DPS attention","message_TW": "远程注意后喷"}],								   
     "s-3201-1000-132-0": [{"type": "text","sub_type": "message","message": "left right ←→","message_TW": "左右喷 ←→" }],			
-    "s-3201-1000-133-0": [{"type": "text","sub_type": "message","message": "Jump (Fast)","message_TW": "击飞快" }],			
-    "s-3201-1000-138-0": [{"type": "text","sub_type": "message","message": "Jump P (Fast)","message_TW": "击飞P快" }],	
+    "s-3201-1000-133-0": [{"type": "text","sub_type": "message","delay": 500,"message": "Jump (Fast)","message_TW": "击飞快" }],			
+    "s-3201-1000-138-0": [{"type": "text","sub_type": "message","delay": 500,"message": "Jump P (Fast)","message_TW": "击飞P快" }],	
     "s-3201-1000-139-0": [{"type": "text","sub_type": "message","message": "Back + Front (Fast)","message_TW": "前后喷（快）↑↓" }],
     "s-3201-1000-141-0": [{"type": "text","class_position":"tank","sub_type": "message","message_TW": "双手前砸 (慢)" }],	
 	"s-3201-1000-142-0": [{"type": "text","class_position":"tank","sub_type": "message","message_TW": "右手+左手"}],	
@@ -339,9 +373,9 @@ module.exports = {
 	{"type": "text","sub_type": "message","delay": 4000,"message": "pull","message_TW": "击退!"}],	
     "s-3201-1000-312-0": [{"type": "text","sub_type": "message","message_TW": "吸血准备!!!" },
 	{"type": "text","sub_type": "message","delay": 2000,"message": "pull","message_TW": "击退!!!"}],
-    "s-3201-1000-313-0": [{"type": "text","sub_type": "msgcp","message": "Circles (Slow)","message_TW": "内外炸圈(慢)"},
+    "s-3201-1000-313-0": [{"type": "text","sub_type": "msgcp","message": "Circles (Slow)","message_TW": "甜甜圈(慢)"},
 	{"type": "func","func": Spawnitem2.bind(null,912,0,75,15,300,6000)}],		
-    "s-3201-1000-314-0": [{"type": "text","sub_type": "message","message": "Circles (Fast)","message_TW": "内外炸圈(快)" },
+    "s-3201-1000-314-0": [{"type": "text","sub_type": "message","message": "Circles (Fast)","message_TW": "甜甜圈快" },
 	{"type": "func","func": Spawnitem2.bind(null,912,0,75,15,300,6000)}],	
 
     //二王
@@ -358,44 +392,56 @@ module.exports = {
     "s-3201-2000-109-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "quaternion Attack","message_TW": "4连击" }],	
     "s-3201-2000-110-0": [{"type": "text","sub_type": "message","message_TW": "飞天" }],			
     "s-3201-2000-114-0": [{"type": "text","sub_type": "message","message_TW": "旋转后方攻击" }],	
-	
-    "s-3201-2000-116-0": [{"type": "text","sub_type": "message","message": "back","message_TW": "后喷" }],	
+   // "s-3201-2000-116-0": [{"type": "text","sub_type": "message","message": "back","message_TW": "后喷" }],	
     "s-3201-2000-150-0": [{"type": "text","sub_type": "message","message_TW": "前冲" }],	
     "s-3201-2000-201-0": [{"type": "text","sub_type": "message","message": "back 8m","message_TW": "向后移动8m" }],	
     "s-3201-2000-202-0": [{"type": "text","sub_type": "message","message": "front 8m","message_TW": "前方移动8m" }],	
     "s-3201-2000-203-0": [{"type": "func","func": skilld_event.bind(null, 203)}],	
     "s-3201-2000-204-0": [{"type": "func","func": skilld_event.bind(null, 204)}],
 	
-   "am-3201-320126-32010224": [{"type": "text","sub_type": "message","message": "next true","message_TW": "接下來吃本体" },{"type": "func","func": skilld_event.bind(null, 32010224)}],	
-   "am-3201-2000-32010220": [{"type": "text","sub_type": "message","message": "next false","message_TW": "接下來吃分身" },{"type": "func","func": skilld_event.bind(null, 32010220)}],		
+   "am-3201-320126-32010224": [{"type": "text","sub_type": "notification","message": "next true","message_TW": "接下來吃本体" },{"type": "func","func": skilld_event.bind(null, 32010224)}],	
+   "am-3201-2000-32010220": [{"type": "text","sub_type": "notification","message": "next false","message_TW": "接下來吃分身" },{"type": "func","func": skilld_event.bind(null, 32010220)}],		
 	"ae-0-0-9203100": [{"type": "func","func": skilld_event.bind(null, 9203100)}],
 	
     "s-3201-2000-211-0": [{"type": "text","sub_type": "message","message": "front","message_TW": "旋转 前" }],	
 
     "s-3201-2000-213-0": [{"type": "text","sub_type": "message","message": "back","message_TW": "后方" }],	
 
-    "s-3201-2000-226-0": [{"type": "text","sub_type": "message","message_TW": "空中吸收 蓄力" }],	
+   // "s-3201-2000-226-0": [{"type": "text","sub_type": "message","message_TW": "空中吸收 蓄力" }],	
     "s-3201-2000-228-0": [ 
        {"type": "text","sub_type": "message","message": "Team up","message_TW": "组员分伤！！" },
        {"type": "text","sub_type": "message","delay": 3500,"message": "dodge","message_TW": "闪"},
        {"type": "text","sub_type": "notification","delay": 3500,"message": "dodge","message_TW": "闪"},
        {"type": "text","sub_type": "message","delay": 65000,"message": "dodge","message_TW": "宝石分伤准备"}],
-    "s-3201-2000-229-0": [{"type": "text","sub_type": "message","message": "3","message_TW": "3个宝石种子传唤" }],	
-    "s-3201-2000-230-0": [{"type": "text","sub_type": "msgcg","message": "AOE","message_TW": "全屏攻击注意沉默" }],
-    "s-3201-2000-231-0": [{"type": "text","sub_type": "msgcp","message": "OUT safe ↓","message_TW": "跑远"},{"type": "func","func": Spawnitem2.bind(null,912,0,0,15,300,3000)}],	
-    "s-3201-2000-232-0": [{"type": "text","sub_type": "msgcp","message": "IN safe ↑","message_TW": "靠近"},
+    "s-3201-2000-229-0": [{"type": "text","sub_type": "message","message": "3","message_TW": "3个宝石炸弹" }],	
+    "s-3201-2000-230-0": [{"type": "text","sub_type": "msgcg","message": "AOE","message_TW": "全屏攻击" }],
+    "s-3201-2000-231-0": [{"type": "text","sub_type": "msgcp","message": "OUT safe ↓","message_TW": "出"},{"type": "func","func": Spawnitem2.bind(null,912,0,0,15,300,3000)}],	
+    "s-3201-2000-232-0": [{"type": "text","sub_type": "msgcp","message": "IN safe ↑","message_TW": "进"},
 	{"type": "func","func": Spawnitem2.bind(null,912,0,0,15,300,3000)},
-	{"type": "func","func": Spawnitem2.bind(null,912,0,0,15,875,3000)}	
+	{"type": "func","func": Spawnitem2.bind(null,912,0,0,15,1000,3000)}	
 	],		
-    "s-3201-2000-233-0": [{"type": "text","sub_type": "message","message": "5","message_TW": "5个宝石种子传唤" }],
+    "s-3201-2000-233-0": [{"type": "text","sub_type": "message","message": "5","message_TW": "5个宝石炸弹" }],
 	
 "h-3201-2000-81": [{"type": "func","func": print_eighty}],	
 "h-3201-2000-78": [{"type": "func","func": start_boss1}],	
 "h-3201-2000-76": [{"type": "func","func": print_seventyfive}],	
 	
 	
-    "s-3201-2000-234-0": [{"type": "text","sub_type": "message","message": "Debuffs","message_TW": "注视" },
-	{"type": "func","func": skilld_event.bind(null, 234)}],
-    "s-3201-2000-235-0": [{"type": "text","sub_type": "message","message": "Debuffs","message_TW": "注视2人吃鉴定" }]	
+    "s-3201-2000-234-0": [{"type": "func","func": skilld_event.bind(null, 234)}],	
+  //  "s-3201-2000-235-0": [{"type": "text","sub_type": "message","message": "Debuffs","message_TW": "注视2人吃鉴定" }]	
+    "s-3201-2000-236-0": [{"type": "text","sub_type": "message","message": "counter","message_TW": "复活反制点名" }],
+	
+	
+   "s-3201-320115-203": [{"type": "func","func": SpawnThing.bind(null,0, 0, 100, 3000)},
+                        {"type": "func","func": Spawnitem2.bind(null,445,0,0,15,100,3000)}
+   ],	// 	1王水晶位
+	
+	//320124-------------302 301
+		
+   "s-3201-320120-204": [{"type": "func","func": SpawnThing5.bind(null,0, 0, 100, 10000)},   //炸弹慢
+                         {"type": "func","func": Spawnitem2.bind(null,445,0,0,15,100,10000)}],
+   
+   "s-3201-320120-205": [{"type": "func","func": SpawnThing6.bind(null,0, 0, 100, 3000)},   //炸弹
+                         {"type": "func","func": Spawnitem2.bind(null,445,0,0,15,100,3000)}]		
 
 };
