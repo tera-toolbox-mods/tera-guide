fb4190150452b8e578cd93cfa901b8e3d04dd1d9
mc <46639858+michengs@users.noreply.github.com>
2019-08-08 08:38:03 +0800

    Add files via upload

---
+++ guides/9054.js
@@ -0,0 +1,47 @@
+
+//made by michengs
+
+let player, entity, library, effect;
+
+
+
+module.exports = {
+	load(dispatch) {
+		({ player, entity, library, effect } = dispatch.require.library);
+	},
+
+"s-454-1000-1103-0": [{"type": "text","sub_type": "message","message": "Head Slam! (Slow)","message_TW": "头砸 (慢)"}],
+"s-454-1000-2103-0": [{"type": "text","sub_type": "message","message": "Head Slam! (Fast)","message_TW": "头砸 (快)"}],
+"s-454-1000-2104-0": [{"type": "text","sub_type": "message","message": "Spin get out (Fast)","message_TW": "旋出（快）"}],
+"s-454-1000-1104-0": [{"type": "text","sub_type": "message","message": "Spin get out (Slow)","message_TW": "旋出（慢）"}],
+"s-454-1000-1201-0": [{"type": "text","sub_type": "message","message": "Body Slam!","message_TW": "身体撞击"}],
+"s-454-1000-2105-0": [{"type": "text","sub_type": "message","message": "Get in","message_TW": "进入"}],
+"s-454-1000-3104-0": [{"type": "text","sub_type": "message","message": "SHIELD!","message_TW": "破盾"}],
+
+
+
+
+"s-454-1001-1104-0": [{"type": "text","sub_type": "message","message": "Frontal Spin!","message_TW": "正面旋转！"}],
+"s-454-1001-2104-0": [{"type": "text","sub_type": "message","message": "Frontal Spin!","message_TW": "正面旋转！"}],
+"s-454-1001-1110-0": [{"type": "text","sub_type": "message","message": "Cyclone! (Slow)","message_TW": "旋风！（慢）"}],
+"s-454-1001-2110-0": [{"type": "text","sub_type": "message","message": "Cyclone! (Fast)","message_TW": "旋风！（快）"}],
+"s-454-1001-2102-0": [{"type": "text","sub_type": "message","message": "Back Flip (Fast)","message_TW": "后翻（快）"}],
+"s-454-1001-1102-0": [{"type": "text","sub_type": "message","message": "Back Flip (Slow)","message_TW": "后翻（慢）"}],
+"s-454-1001-2105-0": [{"type": "text","sub_type": "message","message": "Tail","message_TW": "尾巴"}],
+
+
+
+
+
+
+"s-454-1002-1107-0": [{"type": "text","sub_type": "message","message": "Back Hit! (Slow)","message_TW": "后击！（慢）"}],
+"s-454-1002-2107-0": [{"type": "text","sub_type": "message","message": "Back Hit! (Fast)","message_TW": "后击！（快）"}],
+"s-454-1002-1112-0": [{"type": "text","sub_type": "message","message": "Jump (Slow)","message_TW": "跳跃（慢）"}],
+"s-454-1002-2112-0": [{"type": "text","sub_type": "message","message": "Jump (Fast)","message_TW": "跳跃（快）"}],
+"s-454-1002-3105-0": [{"type": "text","sub_type": "message","message": "Get in","message_TW": "进"}]
+"s-454-1002-3117-0": [{"type": "text","sub_type": "message","message": "In Out In","message_TW": "进 出 进"}],
+"s-454-1002-3110-0": [{"type": "text","sub_type": "message","message": "Pizza mech","message_TW": "披萨机制"}]
+
+		 
+};
+
