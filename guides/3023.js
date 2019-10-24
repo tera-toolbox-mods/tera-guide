654948c349f3a2a2e112669ff701a32f9ca146c3
mc <46639858+michengs@users.noreply.github.com>
2019-10-24 14:14:55 +0800

    Add files via upload

--- guides/3023.js
+++ guides/3023.js
@@ -133,27 +133,32 @@ timer5 = setTimeout(()=>{
 handlers['text']({
 "sub_type": "message",
 "message_TW": "大跳準備喲!",
+"message": "Big jump coming soon！"
 });	
   }, 110000);
 }
 if (skillid === 3119 && debuff === 1 ) {    //紅色气息判断
 handlers['text']({
 "sub_type": "message",
+"message": "OUT",
 "message_TW": "出"
 });	
 } else if (skillid === 3119 && debuff === 2 ) {    //紅色气息判断
 handlers['text']({
 "sub_type": "message",
+"message": "IN",
 "message_TW": "进"
 });	
 } else if (skillid === 3220 && debuff === 1 ) {    //蓝色气息判断
 handlers['text']({
 "sub_type": "message",
+"message": "IN",
 "message_TW": "进"
 });	
 } else if (skillid === 3220 && debuff === 2 ) {    //蓝色气息判断
 handlers['text']({
 "sub_type": "message",
+"message": "OUT",
 "message_TW": "出"
 });	
 }
@@ -189,8 +194,8 @@ clearTimeout(timer4);
 timer4 = setTimeout(()=>{
 handlers['text']({
 "sub_type": "message",
-"message_TW": "4连准备就绪!",
-"message": "debuff coming soon！"
+"message_TW": "quadra kill 准备就绪!",
+"message": "quadra kill coming soon！"
 });	
   }, 70000);
 }
@@ -208,7 +213,7 @@ if(print) {
 handlers['text']({
 "sub_type": "message",
 "message": "80%",
-"message_TW": "4连挥刀准备"
+"message_TW": "quadra kill 准备就绪!"
 });
 }		
 print = false;
@@ -223,75 +228,73 @@ module.exports = {
   //  "h-3201-1000-100": [{"type": "func","func": guid_voice}],
     "h-3023-1000-99": [{"type": "func","func": start_boss}],	 
     "h-3023-1000-80": [{"type": "func","func": start_1boss80}], 
-    "s-3023-1000-104-0": [{"type": "text","sub_type": "message","message":  '_',"message_TW": "点名-跳劈" }],
-    "s-3023-1000-105-0": [{"type": "text","sub_type": "message","message":  '_',"message_TW": "后方攻击" }],
-    "s-3023-1000-110-0": [{"type": "text","sub_type": "message","message":  '_',"message_TW": "晕" },
+    "s-3023-1000-104-0": [{"type": "text","sub_type": "message","message":  'jump',"message_TW": "点名-跳劈" }],
+    "s-3023-1000-105-0": [{"type": "text","sub_type": "message","message":  'back',"message_TW": "后方攻击" }],
+    "s-3023-1000-110-0": [{"type": "text","sub_type": "message","message":  'stun',"message_TW": "晕" },
 	{"type": "func","func": Spawnitem2.bind(null,912,0,50,15,250,6000)}],
-    "s-3023-1000-111-0": [{"type": "text","sub_type": "message","message":  '_',"message_TW": "↓ 左拉" },
+    "s-3023-1000-111-0": [{"type": "text","sub_type": "message","message":  '↓ left pull',"message_TW": "↓ 左拉" },
 						  {"type": "func","func": Spawnitem1.bind(null,553,270,200,180,500,1500)},					
                           {"type": "func","func": Spawnitem1.bind(null,553,270,200,0,300,1500)}],
-    "s-3023-1000-112-0": [{"type": "text","sub_type": "message","message":  '_',"message_TW": "右拉 ↓" },
+    "s-3023-1000-112-0": [{"type": "text","sub_type": "message","message":  'right pull ↓',"message_TW": "右拉 ↓" },
 						  {"type": "func","func": Spawnitem1.bind(null,553,90,200,180,500,1500)},					
                           {"type": "func","func": Spawnitem1.bind(null,553,90,200,0,300,1500)}],
-    "s-3023-1000-113-0": [{"type": "text","sub_type": "message","message":  '_',"message_TW": "↓ 左拉" },
+    "s-3023-1000-113-0": [{"type": "text","sub_type": "message","message":  '↓ left pull',"message_TW": "↓ 左拉" },
 	                      {"type": "func","func": skilld_event.bind(null, 1113)},
 						  {"type": "func","func": Spawnitem1.bind(null,553,270,200,180,500,1500)},					
                           {"type": "func","func": Spawnitem1.bind(null,553,270,200,0,300,1500)}],
-    "s-3023-1000-114-0": [{"type": "text","sub_type": "message","message":  '_',"message_TW": "右拉 ↓" },
+    "s-3023-1000-114-0": [{"type": "text","sub_type": "message","message":  'right pull ↓',"message_TW": "右拉 ↓" },
 	                      {"type": "func","func": skilld_event.bind(null, 1114)},
 						  {"type": "func","func": Spawnitem1.bind(null,553,90,200,180,500,1500)},					
                           {"type": "func","func": Spawnitem1.bind(null,553,90,200,0,300,1500)}],
-    "s-3023-1000-115-0": [{"type": "text","sub_type": "message","message":  '_',"message_TW": "蓄力后扫" },
+    "s-3023-1000-115-0": [{"type": "text","sub_type": "message","message":  'back attack',"message_TW": "蓄力后扫" },
                           {"type": "func","func": Spawnitemsp2.bind(null,1,-2,912,0,0,20,150,100,2000)},
 	                      {"type": "func","func": Spawnitemsp2.bind(null,-2,1,912,0,0,20,150,100,2000)},
                           {"type": "func","func": Spawnitemsp2.bind(null,1,-2,912,0,0,10,300,100,2000)},
 	                      {"type": "func","func": Spawnitemsp2.bind(null,-2,1,912,0,0,10,300,100,2000)}	
 	],
-    "s-3023-1000-116-0": [{"type": "text","sub_type": "message","message":  '_',"message_TW": "爆炸" },
+    "s-3023-1000-116-0": [{"type": "text","sub_type": "message","message":  'get out',"message_TW": "爆炸" },
 	{"type": "func","func": Spawnitem2.bind(null,912,0,0,15,500,6000)}],
 	
-    "am-3023-1000-30231001": [{"type": "text","sub_type": "notification","message": "next true","message_TW": "蓝色debuff" },{"type": "func","func": skilld_event.bind(null, 1001)}],
-    "am-3023-1000-30231000": [{"type": "text","sub_type": "notification","message": "next true","message_TW": "红色debuff" },{"type": "func","func": skilld_event.bind(null, 1000)}],
-	"ae-0-0-99020020": [{"type": "func","func": skilld_event.bind(null, 99020020)},{"type": "text","sub_type": "message","message":  '_',"message_TW": "起来了" }], //复生
-	"ae-0-0-30231000": [{"type": "func","func": skilld_event.bind(null, 30231000)},{"type": "text","sub_type": "notification","message": "next true","message_TW": "red" }],	//开始红色
-	"ae-0-0-30231001": [{"type": "func","func": skilld_event.bind(null, 30231001)},{"type": "text","sub_type": "notification","message": "next true","message_TW": "blue" }],	//开始蓝色	
-    "s-3023-1000-3107-0": [{"type": "text","sub_type": "message","message":  '_',"message_TW": "重击" },
+    "am-3023-1000-30231001": [{"type": "text","sub_type": "notification","message": "debuff blue","message_TW": "蓝色debuff" },{"type": "func","func": skilld_event.bind(null, 1001)}],
+    "am-3023-1000-30231000": [{"type": "text","sub_type": "notification","message": "debuff red","message_TW": "红色debuff" },{"type": "func","func": skilld_event.bind(null, 1000)}],
+	"ae-0-0-99020020": [{"type": "func","func": skilld_event.bind(null, 99020020)},{"type": "text","sub_type": "message","message_TW": "起来了" }], //复生
+	"ae-0-0-30231000": [{"type": "func","func": skilld_event.bind(null, 30231000)},{"type": "text","sub_type": "notification","message": "red","message_TW": "red" }],	//开始红色
+	"ae-0-0-30231001": [{"type": "func","func": skilld_event.bind(null, 30231001)},{"type": "text","sub_type": "notification","message": "blue","message_TW": "blue" }],	//开始蓝色	
+    "s-3023-1000-3107-0": [{"type": "text","sub_type": "message","message":  'smash',"message_TW": "重击" },
 						  {"type": "func","func": Spawnitem1.bind(null,553,90,80,10,1000,4000)},			
 						  {"type": "func","func": Spawnitem1.bind(null,553,270,80,350,1000,4000)}		
 	],
-    "s-3023-1000-3115-0": [{"type": "text","sub_type": "message","message":  '_',"message_TW": "旋转攻击准备" },
+    "s-3023-1000-3115-0": [{"type": "text","sub_type": "message","message":  'spin',"message_TW": "旋转攻击准备" },
 	                       {"type": "func","func": Spawnitem2.bind(null,912,0,0,8,320,3000)}],
-    "s-3023-1000-3116-0": [{"type": "text","sub_type": "message","message":  '_',"message_TW": "旋转攻击" },
+    "s-3023-1000-3116-0": [{"type": "text","sub_type": "message","message":  'spin',"message_TW": "旋转攻击" },
 	                       {"type": "func","func": Spawnitem2.bind(null,912,0,0,8,320,3000)}],
-    "s-3023-1000-3119-0": [{"type": "func","func": skilld_event.bind(null, 3119)},{"type": "text","sub_type": "notification","message": "next true","message_TW": "氣息紅" }],
-    "s-3023-1000-3220-0": [{"type": "func","func": skilld_event.bind(null, 3220)},{"type": "text","sub_type": "notification","message": "next true","message_TW": "氣息藍" }],
-    "s-3023-1000-3223-0": [{"type": "text","sub_type": "message","message":  '_',"message_TW": "紅色诅咒" }],
+    "s-3023-1000-3119-0": [{"type": "func","func": skilld_event.bind(null, 3119)},{"type": "text","sub_type": "notification","message_TW": "氣息紅" }],
+    "s-3023-1000-3220-0": [{"type": "func","func": skilld_event.bind(null, 3220)},{"type": "text","sub_type": "notification","message_TW": "氣息藍" }],
+    "s-3023-1000-3223-0": [{"type": "text","sub_type": "message","message_TW": "紅色诅咒" }],
     // 2王
    	
-    "s-3023-2000-164-0": [{"type": "text","sub_type": "message","message":  '_',"message_TW": "蓄力反击(流血)" }],
-    "s-3023-2000-166-0": [{"type": "text","sub_type": "message","message":  '_',"message_TW": "转身点名" }],
+    "s-3023-2000-164-0": [{"type": "text","sub_type": "message","message":  'Counter attack (bleeding)',"message_TW": "蓄力反击(流血)" }],
+    "s-3023-2000-166-0": [{"type": "text","sub_type": "message","message":  'turn-back',"message_TW": "转身点名" }],
 	
-    "s-3023-2000-175-0": [{"type": "text","sub_type": "message","message":  '_',"message_TW": "沉默" }],
-	
-	
-    "s-3023-2000-178-0": [{"type": "text","sub_type": "message","message":  '_',"message_TW": "毒抓後撓" }],
+    "s-3023-2000-175-0": [{"type": "text","sub_type": "message","message":  'debuff',"message_TW": "沉默" }],
 	
 	
-    "s-3023-2000-181-0": [{"type": "text","sub_type": "message","message":  '_',"message_TW": "插地板(直线)" },
+    "s-3023-2000-178-0": [{"type": "text","sub_type": "message","message":  ' scratching',"message_TW": "毒抓後撓" }],
+	
+	
+    "s-3023-2000-181-0": [{"type": "text","sub_type": "message","message":  'Insert the floor',"message_TW": "插地板(直线)" },
 						  {"type": "func","func": Spawnitem1.bind(null,553,90,80,10,1000,4000)},			
 						  {"type": "func","func": Spawnitem1.bind(null,553,270,80,350,1000,4000)}],
 	
-    "s-3023-2000-182-0": [{"type": "text","sub_type": "message","message":  '_',"message_TW": "踩地(击倒)" }],
+    "s-3023-2000-182-0": [{"type": "text","sub_type": "message","message":  'Knock down',"message_TW": "踩地(击倒)" }],
 	
-    "s-3023-2000-185-0": [{"type": "text","sub_type": "message","message":  '_',"message_TW": "大跳" },
+    "s-3023-2000-185-0": [{"type": "text","sub_type": "message","message":  'big jump',"message_TW": "大跳" },
                           {"type": "func","func": skilld_event.bind(null, 185)},	
 	                      {"type": "func","func": Spawnitem2.bind(null,912,0,0,15,500,6000)},
 	                      {"type": "func","func": Spawnitem2.bind(null,912,0,0,15,750,6000)}],
-   // "s-3023-93935-320-0": [{"type": "text","sub_type": "message","message":  '_',"message_TW": "分身" }],
-  //  "s-3023-93936-322-0": [{"type": "text","sub_type": "message","message":  '_',"message_TW": "偷襲" }],
-     "s-3023-2000-202-0": [{"type": "text","sub_type": "message","message":  '_',"message_TW": "後禽" }],
-     "s-3023-2000-207-0": [{"type": "text","sub_type": "message","message":  '_',"message_TW": "幻影x5(流血)" }],	 
-     "s-3023-2000-212-0": [{"type": "text","sub_type": "message","message":  '_',"message_TW": "闪现(流血)" }],	 
+     "s-3023-2000-202-0": [{"type": "text","sub_type": "message","message":  'BACKSTAB',"message_TW": "後禽" }],
+     "s-3023-2000-207-0": [{"type": "text","sub_type": "message","message":  'Phantom x5(bleed)',"message_TW": "幻影x5(流血)" }],	 
+     "s-3023-2000-212-0": [{"type": "text","sub_type": "message","message":  'Flash (bleed)',"message_TW": "闪现(流血)" }],	 
 	 
  
 	 
