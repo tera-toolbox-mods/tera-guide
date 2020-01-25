9f007f773b4b32aad0d7139a71f5a4243845db70
mc <46639858+michengs@users.noreply.github.com>
2020-01-25 17:09:27 +0800

    Add files via upload

---
+++ guides/3020.js
@@ -0,0 +1,36 @@
+//made by michengs
+
+let player, entity, library, effect;
+
+
+
+module.exports = {
+	load(dispatch) {
+		({ player, entity, library, effect } = dispatch.require.library);
+	},
+
+"s-3020-1000-101-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "跳"}],
+"s-3020-1000-102-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "跳"}],
+"s-3020-1000-103-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "跳"}],
+"s-3020-1000-104-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "跳"}],
+"s-3020-1000-105-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "跳"}],
+"s-3020-1000-106-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "跳"}],
+"s-3020-1000-201-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "跳"}],
+"s-3020-1000-202-0": [{"type": "text","sub_type": "message","message": "Spin","message_TW": "旋转攻击"}],
+
+
+"s-3020-2000-101-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "跳"}],
+"s-3020-2000-102-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "跳"}],
+"s-3020-2000-103-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "跳"}],
+"s-3020-2000-104-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "跳"}],
+"s-3020-2000-105-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "跳"}],
+"s-3020-2000-106-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "跳"}],
+"s-3020-2000-107-0": [{"type": "text","sub_type": "message","message": "Dodge","message_TW": "闪避"}],
+
+
+"s-3020-3000-101-0": [{"type": "text","sub_type": "message","message": "Explo","message_TW": "爆炸"}],
+"s-3020-3000-102-0": [{"type": "text","sub_type": "message","message": "Pull","message_TW": "拉"}],
+"s-3020-3000-105-0": [{"type": "text","sub_type": "message","message": "Dodge","message_TW": "闪避"}],
+"s-3020-3000-110-0": [{"type": "text","sub_type": "message","message": "GET OUT","message_TW": "出"}]
+		 
+};
