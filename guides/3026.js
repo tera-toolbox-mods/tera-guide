2deae856b55db81d553a7ac574c887c76f76f270
mc <46639858+michengs@users.noreply.github.com>
2020-02-28 02:30:46 +0800

    Add files via upload

--- guides/3026.js
+++ guides/3026.js
@@ -1,7 +1,11 @@
 //made by michengs
 
 let player, entity, library, effect;
-
+let	print = true;
+let debuff = 0 ;
+let timer1;
+let timer2;
+let qbacting = false;
 function  applyDistance(loc, distance, degrees) {
         let r = loc.w; //(loc.w / 0x8000) * Math.PI;
      	let	rads = (degrees * Math.PI/180);
@@ -29,7 +33,162 @@ function Spawnitem2(item,degree,distance, intervalDegrees, radius, delay, times,
     }
 }	
 
+function skilld_event(skillid, handlers, event, ent, dispatch) {	
 
+
+if (skillid === 212 && debuff === 1 ) {    //右红内 紅色 
+if (qbacting) {
+handlers['text']({
+"sub_type": "message",
+"message": "OUT",
+"message_TW": "出 | 右手"
+});	
+	} else {
+handlers['text']({
+"sub_type": "message",
+"message": "IN",
+"message_TW": "进 | 右手"
+});					
+	}
+} else if (skillid === 215 && debuff === 1 ) {    //左红内 紅色
+if (qbacting) {
+handlers['text']({
+"sub_type": "message",
+"message": "OUT",
+"message_TW": "出 | 左手"
+});	
+	} else {
+handlers['text']({
+"sub_type": "message",
+"message": "IN",
+"message_TW": "进 | 左手"
+});					
+	}
+} else if (skillid === 213 && debuff === 1 ) {    //左蓝内 紅色
+if (qbacting) {
+handlers['text']({
+"sub_type": "message",
+"message": "IN",
+"message_TW": "进 | 左手"
+});	
+	} else {
+handlers['text']({
+"sub_type": "message",
+"message": "OUT",
+"message_TW": "出 | 左手"
+});					
+	}
+} else if (skillid === 214 && debuff === 1 ) {    //右蓝内 紅色
+if (qbacting) {
+handlers['text']({
+"sub_type": "message",
+"message": "IN",
+"message_TW": "进 | 右手"
+});	
+	} else {
+handlers['text']({
+"sub_type": "message",
+"message": "OUT",
+"message_TW": "出 | 右手"
+});					
+	}
+} else if (skillid === 212 && debuff === 2 ) {    //右红内 蓝色 
+if (qbacting) {
+handlers['text']({
+"sub_type": "message",
+"message": "IN",
+"message_TW": "进 | 右手"
+});	
+	} else {
+handlers['text']({
+"sub_type": "message",
+"message": "OUT",
+"message_TW": "出 | 右手"
+});					
+	}
+} else if (skillid === 215 && debuff === 2 ) {    //左红内 蓝色
+if (qbacting) {
+handlers['text']({
+"sub_type": "message",
+"message": "IN",
+"message_TW": "进 | 左手"
+});	
+	} else {
+handlers['text']({
+"sub_type": "message",
+"message": "OUT",
+"message_TW": "出 | 左手"
+});					
+	}
+} else if (skillid === 213 && debuff === 2 ) {    //左蓝内 蓝色
+if (qbacting) {
+handlers['text']({
+"sub_type": "message",
+"message": "OUT",
+"message_TW": "出 | 左手"
+});	
+	} else {
+handlers['text']({
+"sub_type": "message",
+"message": "IN",
+"message_TW": "进 | 左手"
+});					
+	}
+} else if (skillid === 214 && debuff === 2 ) {    //右蓝内 蓝色
+if (qbacting) {
+handlers['text']({
+"sub_type": "message",
+"message": "OUT",
+"message_TW": "出 | 右手"
+});	
+	} else {
+handlers['text']({
+"sub_type": "message",
+"message": "IN",
+"message_TW": "进 | 右手"
+});					
+	}
+}
+
+
+if (skillid === 3026004) { //愤怒
+qbacting = true;
+}
+if (skillid === 3026005) { //恐惧
+qbacting = false;
+}
+
+if (skillid === 99020020) { //死亡解除debuff
+debuff = 0
+clearTimeout(timer2);
+clearTimeout(timer1);
+}
+if ([3026001].includes(skillid)) {   //debuff为红色
+debuff = 1
+clearTimeout(timer1);
+clearTimeout(timer2);
+timer1 = setTimeout(()=>{
+handlers['text']({
+"sub_type": "message",
+"message_TW": "!",
+});	
+		debuff = 0
+  }, 80000);		   
+}
+if ([3026002].includes(skillid)) {    //debuff为蓝色
+debuff = 2
+clearTimeout(timer2);
+clearTimeout(timer1);
+timer2 = setTimeout(()=>{
+handlers['text']({
+"sub_type": "message",
+"message_TW": "!",
+});	
+		debuff = 0
+  }, 80000);	
+ }
+
+}
 
 module.exports = {
 	load(dispatch) {
@@ -45,17 +204,27 @@ module.exports = {
 "s-3026-1000-153-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "毒尾后扫| 击倒"}],
 "s-3026-1000-154-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "冰风暴| 持续伤害"}],   //30不可防
 "s-3026-1000-155-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "火焰柱| 击倒"}],      //30不可防
-"s-3026-1000-137-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "咆哮击倒"}],
-"s-3026-1000-138-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "AOE"},
-                      {"type": "func","func": Spawnitem2.bind(null,553,0,0,2,1250,200,8000)}]
-//"s-3026-1000-212-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "恐惧"}]//吃同色
+"s-3026-1000-137-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "咆哮击倒"},
+                      {"type": "func","func": Spawnitem2.bind(null,553,0,0,2,1275,200,13000)}],
+"s-3026-1000-138-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "AOE"}],
 
+"qb-3026-1000-3026005": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "吃同色"},{"type": "func","func": skilld_event.bind(null, 3026005)}],//吃同色恐惧
+"qb-3026-1000-3026004": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "吃异色"},{"type": "func","func": skilld_event.bind(null, 3026004)}],//吃异色愤怒
 
-//-------------------qb 3026005   恐惧
-//-------------------qb 3026004   愤怒
+"s-3026-1000-212-0": [{"type": "func","func": skilld_event.bind(null, 212)}],
+"s-3026-1000-141-0": [{"type": "func","func": skilld_event.bind(null, 141)}],   
+"s-3026-1000-215-0": [{"type": "func","func": skilld_event.bind(null, 215)}],      
+"s-3026-1000-144-0": [{"type": "func","func": skilld_event.bind(null, 144)}],
+"s-3026-1000-213-0": [{"type": "func","func": skilld_event.bind(null, 213)}],
+"s-3026-1000-143-0": [{"type": "func","func": skilld_event.bind(null, 143)}],
+"s-3026-1000-214-0": [{"type": "func","func": skilld_event.bind(null, 214)}],
+"s-3026-1000-142-0": [{"type": "func","func": skilld_event.bind(null, 142)}],
 
+"ae-0-0-99020020": [{"type": "func","func": skilld_event.bind(null, 99020020)}],
 
-//30260002寒冰气息
-//30260001火焰气息
+"am-3026-1000-3026001": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "寒冰buff"},{"type": "func","func": skilld_event.bind(null, 3026001)}],//红色
+
+"am-3026-1000-3026002": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "火焰buff"},{"type": "func","func": skilld_event.bind(null, 3026002)}]//蓝色
+
 		 
 };
