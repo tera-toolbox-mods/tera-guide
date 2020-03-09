43b81e57a5029cebc57ad4685a4fca2dcf1dca6a
mc <46639858+michengs@users.noreply.github.com>
2020-03-09 08:27:00 +0800

    Add files via upload

--- guides/3027.js
+++ guides/3027.js
@@ -75,6 +75,7 @@ setTimeout(() => print = true, 15000);
 if (skillid == 351) {
 handlers['text']({
 "sub_type": "message",
+"message": "SHIELD!",
 "message_TW": "破盾"
 });	
  } else {
@@ -124,76 +125,72 @@ module.exports = {
 	
 	
 	
-"s-3027-1001-255-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "！"},{"type": "func","func": Spawnitem1.bind(null,912,0,0,0,1500,5000)}],	//0
-"s-3027-1002-256-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "！"},{"type": "func","func": Spawnitem11.bind(null,912,0,0,0,1500,5000)}],	//60
+"s-3027-1001-255-0": [{"type": "text","sub_type": "message","message": "!","message_TW": "！"},{"type": "func","func": Spawnitem1.bind(null,912,0,0,0,3000,5000)}],	//0
+"s-3027-1002-256-0": [{"type": "text","sub_type": "message","message": "!","message_TW": "！"},{"type": "func","func": Spawnitem11.bind(null,912,0,0,0,3000,5000)}],	//60
 	
 	
 	
-"s-3027-1000-108-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Jump","message_TW": "一刀(慢)"}],
+"s-3027-1000-108-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Strike(slow)","message_TW": "一刀(慢)"}],
 
 
 
 
-"s-3027-1000-112-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "后跳 | 一刀"}],     //连招
-"s-3027-1000-130-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "点名"}],
+"s-3027-1000-112-0": [{"type": "text","sub_type": "message","message": "Back | Strike","message_TW": "后跳 | 一刀"}],     //连招
+"s-3027-1000-130-0": [{"type": "text","sub_type": "message","message": "random aggro","message_TW": "点名"}],
 
-"s-3027-1000-134-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "前戳(转身)"}],    //连招
-"s-3027-1000-134-1": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "后方"}],   
-"s-3027-1000-147-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "后砸"}],
+"s-3027-1000-134-0": [{"type": "text","sub_type": "message","message": "turn around","message_TW": "前戳(转身)"}],    //连招
+"s-3027-1000-134-1": [{"type": "text","sub_type": "message","message": "back","message_TW": "后方"}],   
+"s-3027-1000-147-0": [{"type": "text","sub_type": "message","message": "back","message_TW": "后砸"}],
 
-"s-3027-1000-355-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "下巴粉碎"}],  //连招  右侧上
-"s-3027-1000-114-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "劈擊"}],
+"s-3027-1000-355-0": [{"type": "text","sub_type": "message","message": "Eviscerate","message_TW": "下巴粉碎"}],  //连招  右侧上
+"s-3027-1000-114-0": [{"type": "text","sub_type": "message","message": "Split strike","message_TW": "劈擊"}],
 
-"s-3027-1000-350-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "吸 | 炸圈"},{"type": "text","sub_type": "message","delay": 3750,"message":  'Waves soon...',"message_TW": "進"},{"type": "func","func": Spawnitem2.bind(null,445,0,0,12,240,200,5000)},{"type": "func","func": Spawnitem2.bind(null,445,0,0,8,480,200,5000)}],
-//"s-3027-1000-302-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "炸圈"}],	
+"s-3027-1000-350-0": [{"type": "text","sub_type": "message","message": "Donuts","message_TW": "吸 | 炸圈"},{"type": "text","sub_type": "message","delay": 3750,"message":  'Waves soon...',"message_TW": "進"},{"type": "func","func": Spawnitem2.bind(null,445,0,0,12,240,200,5000)},{"type": "func","func": Spawnitem2.bind(null,445,0,0,8,480,200,5000)}],
 
-"s-3027-1000-357-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "吸 | 远离"}],    //连招
-//"s-3027-1000-110-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "连击"},{"type": "text","sub_type": "MSG","message": "Jump","message_TW": "-110-0"}],
-//"s-3027-1000-110-1": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "连击"},{"type": "text","sub_type": "MSG","message": "Jump","message_TW": "-110-1"}],
-//"s-3027-1000-264-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "大招蓄力"},{"type": "text","sub_type": "MSG","message": "Jump","message_TW": "-264"}],
+
+"s-3027-1000-357-0": [{"type": "text","sub_type": "message","message": "get out","message_TW": "吸 | 远离"}],    //连招
+
 	
-"s-3027-1000-135-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "一刀(慢)"}],	   //连招		
-//"s-3027-1000-130-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "点名挥刀"}],		
+"s-3027-1000-135-0": [{"type": "text","sub_type": "message","message": "Overhand Strike","message_TW": "一刀(慢)"}],	   //连招		
 	
-"s-3027-1000-111-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "眩晕 | 一刀"}],	   //连招  左侧		
-//"s-3027-1000-130-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "点名挥刀"}],	
 	
-"s-3027-1000-136-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "2圈 | 一刀"}],	   //连招
-"s-3027-1000-144-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "挥刀"}],		
-//"s-3027-1000-130-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "点名挥刀"}],		
-	
-"s-3027-1000-356-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "瞬移(点名)"}], ////连招	点名					  
-//"s-3027-1000-356-1": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "瞬移"}],
-//"s-3027-1000-147-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "后砸"}],		
-	
-"s-3027-1000-117-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "瞬移(点名)"}],//连招	 随机
-//"s-3027-1000-117-1": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "瞬移"}],
-//"s-3027-1000-130-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "瞬移"}],
+"s-3027-1000-111-0": [{"type": "text","sub_type": "message","message": "stun | Strike","message_TW": "眩晕 | 一刀"}],	   //连招  左侧		
 
-"s-3027-1000-145-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "3圈 | 斩击"}], //连招  3连挥刀 
-"s-3027-1000-139-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "！"}],
+	
+"s-3027-1000-136-0": [{"type": "text","sub_type": "message","message": "2x360°","message_TW": "2圈 | 一刀"}],	   //连招
+"s-3027-1000-144-0": [{"type": "text","sub_type": "message","message": "Sword","message_TW": "挥刀"}],		
+	
+	
+"s-3027-1000-356-0": [{"type": "text","sub_type": "message","message": "Teleport","message_TW": "瞬移(点名)"}], ////连招	点名					  
+	
+	
+"s-3027-1000-117-0": [{"type": "text","sub_type": "message","message": "Teleport","message_TW": "瞬移(点名)"}],//连招	 随机
+
+
+"s-3027-1000-145-0": [{"type": "text","sub_type": "message","message": "3x360°","message_TW": "3圈 | 斩击"}], //连招  3连挥刀 
+"s-3027-1000-139-0": [{"type": "text","sub_type": "message","message": "！","message_TW": "！"}],
 "s-3027-1000-140-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "大跳砸 | 完美格挡"},{"type": "func","func": Spawnitem2.bind(null,445,0,180,8,460,200,3000)}],	
 
-"s-3027-1000-151-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "三斩 | 一刀"}],  //连招  2次点
-"s-3027-1000-149-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "点名"}],
-"s-3027-1000-149-1": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "后方瞬移"}],
-"s-3027-1000-148-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "点名"}],
-"s-3027-1000-148-1": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "瞬移"}],
+"s-3027-1000-151-0": [{"type": "text","sub_type": "message","message": "Three chop","message_TW": "三斩 | 一刀"}],  //连招  2次点
+"s-3027-1000-149-0": [{"type": "text","sub_type": "message","message": "random aggro","message_TW": "点名"}],
+"s-3027-1000-149-1": [{"type": "text","sub_type": "message","message": "back Teleport","message_TW": "后方瞬移"}],
+"s-3027-1000-148-0": [{"type": "text","sub_type": "message","message": "random aggro","message_TW": "点名"}],
+"s-3027-1000-148-1": [{"type": "text","sub_type": "message","message": "Teleport","message_TW": "瞬移"}],
 
-"s-3027-1000-141-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "回旋 | 一刀"}], //连招  3连挥刀 
-"s-3027-1000-146-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "下巴 | 一刀"}],
+"s-3027-1000-141-0": [{"type": "text","sub_type": "message","message": "round | Strike ","message_TW": "回旋 | 一刀"}], //连招  3连挥刀 
+"s-3027-1000-146-0": [{"type": "text","sub_type": "message","message": "Eviscerate | Strike","message_TW": "下巴 | 一刀"}],
 				  
-"s-3027-1000-142-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "下巴 | 一刀"}], //连招  3连挥刀 
-"s-3027-1000-143-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "一刀"}],					  
+"s-3027-1000-142-0": [{"type": "text","sub_type": "message","message": "Eviscerate | Strike","message_TW": "下巴 | 一刀"}], //连招  3连挥刀 
+"s-3027-1000-143-0": [{"type": "text","sub_type": "message","message": "Overhand Strike","message_TW": "一刀"}],					  
 					  
 "s-3027-1000-116-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "大跳 | 完美格挡"}],	
-"s-3027-1000-116-1": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "闪"},{"type": "func","func": Spawnitem2.bind(null,445,0,180,8,460,200,3000)}],
+"s-3027-1000-116-1": [{"type": "text","sub_type": "message","message": "dodge","message_TW": "闪"},{"type": "func","func": Spawnitem2.bind(null,445,0,180,8,460,200,3000)}],
 
 "s-3027-1000-402-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "大跳| 插地"}],
-"s-3027-1000-109-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "前跳 | 一刀"}],
+"s-3027-1000-109-0": [{"type": "text","sub_type": "message","message": "Forward Jump","message_TW": "前跳 | 一刀"}],
 					  
-"s-3027-1000-351-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "破盾"},{"type": "func","func": skilld_event.bind(null, 351)}],
+"s-3027-1000-351-0": [{"type": "text","sub_type": "message","message": "SHIELD!","message_TW": "破盾"},{"type": "func","func": skilld_event.bind(null, 351)}],
 
-"s-3027-1000-401-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "全屏攻击"}]				
+"s-3027-1000-401-0": [{"type": "text","sub_type": "message","message": "AOE","message_TW": "全屏攻击"}]				
 	 
 };
