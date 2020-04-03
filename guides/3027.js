af49da6635698005a272127115dddf5e2f8b6d69
mc <46639858+michengs@users.noreply.github.com>
2020-04-04 00:58:49 +0800

    Add files via upload

--- guides/3027.js
+++ guides/3027.js
@@ -7,65 +7,10 @@ let notices = true;
 let print = true;
 let printHP = true;
 let isHP_69_39 = false;
-function  applyDistance(loc, distance, degrees) {
-        let r = loc.w; //(loc.w / 0x8000) * Math.PI;
-     	let	rads = (degrees * Math.PI/180);
-	    let	finalrad = r - rads;
-        loc.x += Math.cos(finalrad) * distance;
-        loc.y += Math.sin(finalrad) * distance;
-        return loc;
-}
+
+const {SpawnMarker, SpawnItem, SpawnVector, SpawnCircle, SpawnSemicircle, SpawnObject, applyDistance} = require("../lib");
 
 
-	//构建圆形范围提示    （提示标志  偏移角度 偏移距离 间隔 半径 延迟 时间）
-function Spawnitem2(item,degree,distance, intervalDegrees, radius, delay, times, handlers, event, entity ) {
-	let shield_loc = entity['loc'].clone();
-	shield_loc.w = entity['loc'].w;
-    let	degrees = 360 - degree;	
-	applyDistance(shield_loc, distance, degrees);
-    for (let angle = -Math.PI; angle <= Math.PI; angle +=  Math.PI * intervalDegrees / 180) {
-        handlers['spawn']({
-        	"id": item,
-			"delay": delay,			
-        	"sub_delay": times,
-        	"distance": radius,
-        	"offset": angle
-        }, {loc: shield_loc});
-    }
-}	
-	//构建直线（提示标志 偏移角度 偏移距离  角度 最远距离   时间）
-function Spawnitem1(item,degree,distance,angles, maxRadius, times, handlers, event, entity) {
-	
-	let shield_loc = entity['loc'].clone();
-	shield_loc.w = entity['loc'].w;	
-    let	degrees = 360 - degree;	
-	applyDistance(shield_loc, distance, degrees);		
-    let angle = angles * Math.PI/180
-    for (let radius=50 ; radius<=maxRadius; radius+=50) {
-        handlers['spawn']({
-        	"id": item,
-        	"sub_delay": times,
-        	"distance": radius,
-        	"offset": angle
-        }, {loc: shield_loc});
-    }
-}
-function Spawnitem11(item,degree,distance,angles, maxRadius, times, handlers, event, entity) {
-	
-	let shield_loc = entity['loc'].clone();
-	shield_loc.w = entity['loc'].w;	
-    let	degrees = 360 - degree;	
-	applyDistance(shield_loc, distance, degrees);		
-    let angle = angles * Math.PI/180
-    for (let radius=50 ; radius<=maxRadius; radius+=50) {
-        handlers['spawn']({
-        	"id": item,
-        	"sub_delay": times,
-        	"distance": radius,
-        	"offset": angle
-        }, {loc: shield_loc});
-    }
-}
 
 function skilld_event(skillid, handlers, event, ent, dispatch) {	
 
@@ -126,19 +71,19 @@ module.exports = {
 	//(distance, angle, duration, type, offsetAngle, offsetDistance, minRadius, maxRadius, rotateAngle, rotateRadius) 
 	
 
-"h-3027-1000-90": [{"type": "func","func": skilld_event.bind(null, 90)}],	
-"h-3027-1000-60": [{"type": "func","func": skilld_event.bind(null, 60)}],	
-"h-3027-1000-30": [{"type": "func","func": skilld_event.bind(null, 30)}],	
+"h-3027-1000-89": [{"type": "func","func": skilld_event.bind(null, 90)}],	
+"h-3027-1000-59": [{"type": "func","func": skilld_event.bind(null, 60)}],	
+"h-3027-1000-29": [{"type": "func","func": skilld_event.bind(null, 30)}],	
 "h-3027-1000-69": [{"type": "func","func": skilld_event.bind(null, 69)}],	
 "h-3027-1000-39": [{"type": "func","func": skilld_event.bind(null, 39)}],	
 	
 	
 	
 	
-"s-3027-1001-255-0": [{"type": "func","func": Spawnitem1.bind(null,912,0,0,0,3000,5000)}],	//0
-"s-3027-1002-256-0": [{"type": "func","func": Spawnitem11.bind(null,912,0,0,0,3000,5000)}],	//60	
-"s-3027-1003-257-0": [{"type": "func","func": Spawnitem1.bind(null,912,0,0,0,3000,5000)}],	//0
-"s-3027-1004-258-0": [{"type": "func","func": Spawnitem11.bind(null,912,0,0,0,3000,5000)}],	//60	
+	"s-3027-1001-255-0": [{"type": "func","func": SpawnVector.bind(null,553,0,0,0,3000,0,5000)}], //0
+	"s-3027-1002-256-0": [{"type": "func","func": SpawnVector.bind(null,553,0,0,0,3000,0,5000)}], //60
+	"s-3027-1003-257-0": [{"type": "func","func": SpawnVector.bind(null,553,0,0,0,3000,0,5000)}], //0
+	"s-3027-1004-258-0": [{"type": "func","func": SpawnVector.bind(null,553,0,0,0,3000,0,5000)}], //60
 	
 "s-3027-1000-108-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Strike(slow)","message_TW": "一刀(慢)"}],
 
@@ -155,10 +100,17 @@ module.exports = {
 //"s-3027-1000-355-0": [{"type": "text","sub_type": "message","message": "Eviscerate","message_TW": "下巴粉碎"}],  //连招  右侧上
 "s-3027-1000-114-0": [{"type": "text","sub_type": "message","message": "Split strike","message_TW": "劈擊"}],
 
-"s-3027-1000-350-0": [{"type": "text","sub_type": "message","message": "Donuts","message_TW": "吸 | 炸圈"},{"type": "text","sub_type": "message","delay": 3750,"message":  'Waves soon...',"message_TW": "進"},{"type": "func","func": Spawnitem2.bind(null,445,0,0,12,240,200,5000)},{"type": "func","func": Spawnitem2.bind(null,445,0,0,8,480,200,5000)},{"type": "func","func": Spawnitem2.bind(null,445,0,0,3,950,200,5000)}],
+"s-3027-1000-350-0": [{"type": "text","sub_type": "message","message": "Donuts","message_TW": "吸 | 炸圈"},
+        {"type": "text","sub_type": "message","delay": 3750,"message":  'Waves soon...',"message_TW": "進"},
+		{"type": "func","func": SpawnCircle.bind(null,false,445,0,0,12,240,0,5000)},
+		{"type": "func","func": SpawnCircle.bind(null,false,445,0,0,8,480,0,5000)},
+		{"type": "func","func": SpawnCircle.bind(null,false,445,0,0,3,950,0,5000)},
+		{"type": "func","func": skilld_event.bind(null, 350)}],
 
 
-"s-3027-1000-357-0": [{"type": "text","sub_type": "message","message": "get out","message_TW": "吸 | 远离"}],    //连招
+"s-3027-1000-357-0": [{"type": "text","sub_type": "message","message": "get out","message_TW": "吸 | 远离"},
+		{"type": "func","func": SpawnCircle.bind(null,false,553,0,0,20,500,2000,5000)},
+		{"type": "func","func": skilld_event.bind(null, 357)}],    //连招
 
 	
 "s-3027-1000-135-0": [{"type": "text","sub_type": "message","message": "Overhand Strike","message_TW": "一刀(慢)"}],	   //连招		
@@ -178,8 +130,8 @@ module.exports = {
 
 
 "s-3027-1000-145-0": [{"type": "text","sub_type": "message","message": "3x360°","message_TW": "3圈 | 斩击"}], //连招  3连挥刀 
-"s-3027-1000-139-0": [{"type": "text","sub_type": "message","message": "！","message_TW": "！"}],
-"s-3027-1000-140-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "大跳砸"},{"type": "func","func": Spawnitem2.bind(null,445,0,180,8,460,200,3000)}],	
+"s-3027-1000-139-0": [{"type": "text","sub_type": "message","message": "！","message_TW": "！"},{"type": "func","func": SpawnCircle.bind(null,true,413,0,180,8,660,1000,2000)}],
+"s-3027-1000-140-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "大跳砸"},{"type": "func","func": SpawnCircle.bind(null,true,912,0,180,8,460,0,3000)}],	
 
 "s-3027-1000-151-0": [{"type": "text","sub_type": "message","message": "Three chop","message_TW": "三斩 | 一刀"}],  //连招  2次点
 //"s-3027-1000-149-0": [{"type": "text","sub_type": "message","message": "random aggro","message_TW": "点名"}],
@@ -193,8 +145,8 @@ module.exports = {
 "s-3027-1000-142-0": [{"type": "text","sub_type": "message","message": "Eviscerate | Strike","message_TW": "下巴 | 一刀"}], //连招  3连挥刀 
 "s-3027-1000-143-0": [{"type": "text","sub_type": "message","message": "Overhand Strike","message_TW": "一刀"}],					  
 					  
-"s-3027-1000-116-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "大跳"}],	
-"s-3027-1000-116-1": [{"type": "text","sub_type": "message","message": "dodge","message_TW": "闪"},{"type": "func","func": Spawnitem2.bind(null,445,0,180,8,460,200,3000)}],
+"s-3027-1000-116-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "大跳"},{"type": "func","func": SpawnCircle.bind(null,true,413,0,180,8,560,0,1000)}],	
+//"s-3027-1000-116-1": [{"type": "text","sub_type": "message","message": "dodge","message_TW": "闪"},{"type": "func","func": SpawnCircle.bind(null,true,912,0,180,8,460,0,3000)}],
 
 "s-3027-1000-402-0": [{"type": "text","sub_type": "message","message": "Jump","message_TW": "大跳| 插地"}],
 "s-3027-1000-109-0": [{"type": "text","sub_type": "message","message": "Forward Jump","message_TW": "前跳 | 一刀"}],
