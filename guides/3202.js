c8ca112f7b57597be45dc46b87f026953ff6b980
Kasea <kaseaa@tromkom.no>
2020-06-18 08:50:04 +0200

    added DAHM basic guide

---
+++ guides/3202.js
@@ -0,0 +1,29 @@
+// DAHM -- Written by Vanq
+
+module.exports = {
+
+	// Out>In
+    "s-3202-1000-2124-0": [{"type": "text","sub_type": "notification","message": " Out>In"}],
+    "s-3202-1000-1124-0": [{"type": "text","sub_type": "notification","message": " Out>In"}],
+	
+	// In>Out
+    "s-3202-1000-2121-0": [{"type": "text","sub_type": "notification","message": " In>Out"}],
+    "s-3202-1000-1121-0": [{"type": "text","sub_type": "notification","message": " In>Out"}],
+	
+	// Back Push
+	"s-3202-1000-2112-0": [{"type": "text","sub_type": "notification","message": " Back Push"}],
+    "s-3202-1000-1112-0": [{"type": "text","sub_type": "notification","message": " Back Push"}],
+	
+	// Front Stun
+	"s-3202-1000-2105-0": [{"type": "text","sub_type": "notification","message": " Front Stun"}],
+    "s-3202-1000-1105-0": [{"type": "text","sub_type": "notification","message": " Front Stun"}],
+	
+	// Stomp & Spin
+	"s-3202-1000-2109-0": [{"type": "text","sub_type": "notification","message": " Stomp & Spin"}],
+    "s-3202-1000-1109-0": [{"type": "text","sub_type": "notification","message": " Stomp & Spin"}],
+	
+	// Donut-Wave
+	"s-3202-1000-2110-0": [{"type": "text","sub_type": "notification","message": " Donut-Wave"}],
+    "s-3202-1000-1110-0": [{"type": "text","sub_type": "notification","message": " Donut-Wave"}]
+
+};
